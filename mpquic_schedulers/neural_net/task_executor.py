2f46cd48643bd9aac74830da63bd8776246df2b9
Sai Sharan Tangeda <saisarantangeda@gmail.com>
2021-04-01 11:03:09 +0530

    switch from cmd arguments to function parameters

--- mpquic_schedulers/neural_net/task_executor.py
+++ mpquic_schedulers/neural_net/task_executor.py
@@ -1,4 +1,3 @@
-import argparse
 import logging
 import os
 import time
@@ -7,16 +6,8 @@ from datetime import datetime
 from mpquic_schedulers.neural_net.train_save import train_save_model
 
 
-def train_update_model():
+def train_update_model(training_file, epochs, output_dir):
     project_home_dir = os.getenv("PROJECT_HOME_DIR")
-    
-    parser = argparse.ArgumentParser(description='Train & Save Model')
-    parser.add_argument('--epochs', type=int, dest="epochs", help="Num of epochs to run", default=3)
-    parser.add_argument('--training_file', type=str, dest="training_file", help="Absolute Path to training file",
-                        required=True)
-    parser.add_argument('--output_dir', type=str, dest="output_dir", help="Absolute Path to output dir", required=True)
-    args = parser.parse_args()
-    
     logs_dir = os.path.join(project_home_dir, "nn_logs")
     os.makedirs(logs_dir, exist_ok=True)
     
@@ -34,6 +25,6 @@ def train_update_model():
             time.sleep(2 * 60)
             continue
         logging.info("Starting to Train & save the model")
-        train_save_model(args.training_file, args.epochs, args.output_dir)
+        train_save_model(training_file, epochs, output_dir)
         time.sleep(5 * 60)
 
